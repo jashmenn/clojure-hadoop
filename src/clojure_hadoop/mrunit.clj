473dc437f19a1303641d4f26027ba4b883d2cca9
Nate Murray <nate@natemurray.com>
2011-02-24 15:46:07 -0800

    changed the test API to support JobConf

--- src/clojure_hadoop/mrunit.clj
+++ src/clojure_hadoop/mrunit.clj
@@ -4,6 +4,7 @@
   (:import [org.apache.hadoop.mrunit MapDriver ReduceDriver MapReduceDriver]))
 
 (imp/import-io)
+(imp/import-conf)
 (imp/import-mapred)
 (imp/import-mapred-lib)
 
@@ -18,25 +19,38 @@
 (defn set-input-values [driver values]
   (doall (map (fn [value] (.withInputValue driver value)) values)))
 
-(defn test-mapper [mapper input & expected]
-  (let [driver (MapDriver. mapper)]
-    (doto driver
-      (.withInput  (first input) (second input))
-      (set-expected-outputs expected)
-      (.runTest))))
+(defn test-mapper 
+  ([mapper inputs expected]
+     (test-mapper mapper inputs expected (JobConf.)))
+  ([mapper inputs expected jobconf]
+     (.configure mapper jobconf)
+     (let [driver (MapDriver. mapper)]
+       (doto driver
+         (set-inputs inputs)
+         (set-expected-outputs expected)
+         (.runTest)))))
 
-(defn test-reducer [reducer input-key input-values & expected]
-  (let [driver (ReduceDriver. reducer)]
-    (doto driver
-      (.withInputKey input-key)
-      (set-input-values input-values)
-      (set-expected-outputs expected)
-      (.runTest))))
+(defn test-reducer 
+  ([reducer input-key input-values expected]
+     (test-reducer reducer input-key input-values expected (JobConf.)))
+  ([reducer input-key input-values expected jobconf]
+     (.configure reducer jobconf)
+     (let [driver (ReduceDriver. reducer)]
+       (doto driver
+         (.withInputKey input-key)
+         (set-input-values input-values)
+         (set-expected-outputs expected)
+         (.runTest)))))
 
-(defn test-map-reduce [mapper reducer inputs expected]
-  (let [driver (MapReduceDriver. mapper reducer)]
-    (doto driver
-      (set-inputs inputs)
-      (set-expected-outputs expected)
-      (.runTest))))
+(defn test-map-reduce
+  ([mapper reducer inputs expected]
+     test-map-reduce mapper reducer inputs expected (JobConf.))
+  ([mapper reducer inputs expected jobconf]
+     (.configure mapper jobconf)
+     (.configure reducer jobconf)
+     (let [driver (MapReduceDriver. mapper reducer)]
+       (doto driver
+         (set-inputs inputs)
+         (set-expected-outputs expected)
+         (.runTest)))))
 
--- test/clojure_hadoop/test/mrunit.clj
+++ test/clojure_hadoop/test/mrunit.clj
@@ -14,34 +14,34 @@
 
 (deftest test-identity-mapper 
   (test-mapper (IdentityMapper.)
-               [(text "foo") (text "bar")]
-               [(text "foo") (text "bar")]))
+               [[(text "foo") (text "bar")]]
+               [[(text "foo") (text "bar")]]))
 
 (deftest test-inverse 
   (test-mapper (InverseMapper.)
-               [(text "foo") (text "bar")]
-               [(text "bar") (text "foo")]))
+               [[(text "foo") (text "bar")]]
+               [[(text "bar") (text "foo")]]))
 
 (deftest test-token-count 
   (test-mapper (TokenCountMapper.)
-               [(text "foo") (text "my dog has fleas")]
-               [(text "my") (longw 1)]
-               [(text "dog") (longw 1)]
-               [(text "has") (longw 1)]
-               [(text "fleas") (longw 1)]))
+               [[(text "foo") (text "my dog has fleas")]]
+               [[(text "my") (longw 1)]
+                [(text "dog") (longw 1)]
+                [(text "has") (longw 1)]
+                [(text "fleas") (longw 1)]]))
 
 (deftest test-identity-reducer
   (test-reducer (IdentityReducer.) (text "foo")
                 [(text "bar") 
                  (text "baz")]
-                [(text "foo") (text "bar")]
-                [(text "foo") (text "baz")]))
+                [[(text "foo") (text "bar")]
+                 [(text "foo") (text "baz")]]))
 
 (deftest test-sum
   (test-reducer (LongSumReducer.) (text "foo")
                 [(longw 5) 
                  (longw 4)]
-                [(text "foo") (longw 9)]))
+                [[(text "foo") (longw 9)]]))
 
 (deftest test-word-count
   (test-map-reduce (TokenCountMapper.) (LongSumReducer.) 
@@ -52,3 +52,22 @@
       [(text "fleas") (longw 2)]
       [(text "has") (longw 2)]
       [(text "my")  (longw 2)]]))
+
+(deftest test-jobconf
+  (def jobconf 
+       (doto (JobConf.)
+         (.set "foo.bar.baz" "hello")))
+
+  (defn my-mapper []
+    (proxy [IdentityMapper] [] 
+      (configure [jobconf] 
+        (is (= "hello" (.get jobconf "foo.bar.baz"))))))
+
+  (test-map-reduce (my-mapper) (IdentityReducer.) 
+     [[(text "k") (text "a")]
+      [(text "k") (text "b")]]
+     [[(text "k") (text "a")]
+      [(text "k") (text "b")]]
+     jobconf))
+
+
